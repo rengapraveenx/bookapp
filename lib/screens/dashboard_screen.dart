5c3723ef860e73aa46ce0c7400bba03485954a12
renga <abi129.off@gmail.com>
2026-05-08 07:08:20 +0530

    Move search bar and navigation to AppBar bottom

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -22,65 +22,84 @@ class _DashboardScreenState extends State<DashboardScreen> {
             onPressed: () {},
           ),
         ],
+        bottom: _DashboardBottom(
+          selectedIndex: _selectedIndex,
+          onIndexChanged: (index) => setState(() => _selectedIndex = index),
+        ),
       ),
       body: Center(
         child: Text('Dashboard Content $_selectedIndex'),
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Theme.of(context).colorScheme.surfaceContainerHighest,
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none,
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                ),
+    );
+  }
+}
+
+class _DashboardBottom extends StatelessWidget implements PreferredSizeWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onIndexChanged;
+
+  const _DashboardBottom({
+    required this.selectedIndex,
+    required this.onIndexChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: TextFormField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Theme.of(context).colorScheme.surfaceContainerHighest,
+              prefixIcon: const Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
               ),
+              contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.home,
-                    color: _selectedIndex == 0 ? Theme.of(context).colorScheme.primary : Colors.grey,
-                  ),
-                  onPressed: () => setState(() => _selectedIndex = 0),
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.shopping_cart,
-                    color: _selectedIndex == 1 ? Theme.of(context).colorScheme.primary : Colors.grey,
-                  ),
-                  onPressed: () => setState(() => _selectedIndex = 1),
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.search,
-                    color: _selectedIndex == 2 ? Theme.of(context).colorScheme.primary : Colors.grey,
-                  ),
-                  onPressed: () => setState(() => _selectedIndex = 2),
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.person,
-                    color: _selectedIndex == 3 ? Theme.of(context).colorScheme.primary : Colors.grey,
-                  ),
-                  onPressed: () => setState(() => _selectedIndex = 3),
-                ),
-              ],
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Icon(
+                Icons.home,
+                color: selectedIndex == 0 ? Theme.of(context).colorScheme.primary : Colors.grey,
+              ),
+              onPressed: () => onIndexChanged(0),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: selectedIndex == 1 ? Theme.of(context).colorScheme.primary : Colors.grey,
+              ),
+              onPressed: () => onIndexChanged(1),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: selectedIndex == 2 ? Theme.of(context).colorScheme.primary : Colors.grey,
+              ),
+              onPressed: () => onIndexChanged(2),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.person,
+                color: selectedIndex == 3 ? Theme.of(context).colorScheme.primary : Colors.grey,
+              ),
+              onPressed: () => onIndexChanged(3),
             ),
           ],
         ),
-      ),
+      ],
     );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(120);
 }
