842180477fdf21ec31a6b2402c0240c83a1717ec
renga <abi129.off@gmail.com>
2026-05-08 09:05:27 +0530

    Revert "Remove spacing between All magazines title and grid items"
    This reverts commit 1a82b5527b91193ce9b1070c1cd0ce25ed2f6847.

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -179,48 +179,37 @@ class _AllMagazines extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text(
-              'All magazines',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
-                height: 1.0,
-              ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'All magazines',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
             ),
-            GestureDetector(
-              onTap: () {},
-              child: const Icon(
-                Icons.arrow_forward_ios,
-                color: Colors.white,
-                size: 16,
-              ),
-            ),
-          ],
-        ),
-        Transform.translate(
-          offset: const Offset(0, -12),
-          child: Wrap(
-            spacing: 10,
-            runSpacing: 10,
-            children: _magazineImages.map((image) {
-              return SizedBox(
-                width: (MediaQuery.of(context).size.width - 42) / 2,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.asset(image, fit: BoxFit.cover),
-                ),
-              );
-            }).toList(),
           ),
-        ),
-      ],
+          GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemCount: _magazineImages.length,
+            itemBuilder: (context, index) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(_magazineImages[index], fit: BoxFit.cover),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
